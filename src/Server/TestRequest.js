b97d571f1c4dce277069aa7ab973f8ca477d14fc
MostafaHMK <mostafahmk@gmail.com>
2021-12-31 00:11:46 +0200

    working get and post request

--- src/Server/TestRequest.js
+++ src/Server/TestRequest.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React from "react";
 
-//let baseURL = "http://e1e5-102-188-120-135.ngrok.io/movie/61c73770017b0cbec6c84133";
-const baseURL = "http://localhost:3000/sign-in";
+let baseURL = "http://e1e5-102-188-120-135.ngrok.io/movie/61c73770017b0cbec6c84133";
+//const baseURL = "http://localhost:3000/sign-in";
+//let baseURL = "http://e1e5-102-188-120-135.ngrok.io/sign-in";
 const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers":
@@ -13,10 +14,11 @@ export default function TestRequests() {
 
   React.useEffect(() => {
     axios
-      .post(baseURL, {
-        password: "tEsTa!237T",
-        email: "Khaled@gmail.com",
-      },)
+      .get(baseURL, {
+        headers: {
+          Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxYzY1MDU1YTRjZTZlN2Q2NmEwMzlmMSIsImlhdCI6MTY0MDkwMDYwOCwiZXhwIjoxNjQwOTA0MjA4fQ.oM4tc9T_HRhncELa8Kx6WyMu66kFTeRquIwexegO2qU"
+        }
+      })
       .then((response) => {
         setPost(response.data);
         console.log(response.data);
@@ -35,3 +37,6 @@ export default function TestRequests() {
     </div>
   );
 }
+/*
+.post(baseURL)
+*/
