41b605c555db421219f0b24ca6bc28c40b054e9e
MostafaHMK <mostafahmk@gmail.com>
2021-12-30 03:04:53 +0200

    Added saving reservations in state

--- src/Components/Seats.js
+++ src/Components/Seats.js
@@ -1,17 +1,50 @@
 import React, {useState} from 'react'
 export default function Seats(props) {
-    const [hoverButton, setHoverButton] = useState(false);
     const seats = []
+    const [reserved,setReserved] = useState([])
     var count = 0
+    function handleHover (e) {
+        console.log(e.target.style.background)
+        if (e.target.style.background == 'grey' || e.target.style.background == '')
+        {
+            e.target.style.background = 'yellow'
+        }
+    }
+    function handleHoverLeave (e) {
+        console.log(e.target.id)
+        if (e.target.style.background == 'yellow')
+        {
+            e.target.style.background = 'grey'
+        }
+            
+    }
+    function handleClick(e) {
+        console.log(e.target.style.background)
+        if (e.target.style.background == 'rgb(255, 154, 60)')
+        {
+            e.target.style.background = 'grey'
+            const newReserved = reserved.filter((item) => item != e.target.id)
+            setReserved(newReserved)
+        }
+        else
+        {
+            e.target.style.background = '#ff9a3c'
+            const newReserved = [...reserved]
+            newReserved.push(e.target.id)
+            setReserved(newReserved)
+        }
+        console.log(reserved)
+        
+    }
     for (const seat of props.seatt)
     {
       if (seat == 1)
       {
-          seats.push(<button id={count} onMouseLeave={() =>setHoverButton(true)} style={styles.notbutton}></button>)
+          seats.push(<button id={count} key={count} style={styles.notbutton}></button>)
       }
       else 
       {
-          seats.push(<button id={count} onMouseLeave={() =>setHoverButton(true)} style={hoverButton == true ? styles.button : styles.clickedbutton}></button>)
+          seats.push(<button id={count} key={count} onMouseEnter={handleHover} onMouseLeave={handleHoverLeave} onClick={handleClick} style={styles.button}></button>)
           
       }
       count++
